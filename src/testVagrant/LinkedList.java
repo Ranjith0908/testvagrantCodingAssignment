4a2d1831eb08d2caa8447de2c0b805245e6b74ad
Ranjith <ranjith98209@gmail.com>
2023-08-16 12:39:19 +0530

    code

---
+++ src/testVagrant/LinkedList.java
@@ -0,0 +1,74 @@
+package testVagrant;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class LinkedList {
+	Node head;
+	class Node{
+		String data;	
+		Node next;
+		public String val;
+		Node(String val){
+			data = val;
+			next = null;
+		}
+	}
+	 LinkedList(){
+		head = null;
+	}
+	public void insertatBeginning(String val) {
+		Node temp = head;
+		int count=1;
+		while(temp!=null && count<3) {
+			count++;
+		//	System.out.print(count);
+			if(count==3) {
+				temp.next= null;
+			}
+			temp=temp.next;
+		}
+		Node newNode = new Node(val);
+		
+	
+		if(head==null) {
+			head = newNode;
+		}
+			else {
+				newNode.next = head;
+				head = newNode;
+		}
+	}
+	static void removeDuplicate(Node head)
+    {
+        
+        HashSet<String> duplicate = new HashSet<>();
+        Node current = head;
+        Node previous = null;
+        while (current != null) {
+            String current1 = current.val;
+            
+            if (duplicate.contains(current1)) {
+                previous.next = current.next;
+            }
+            else {
+                duplicate.add(current1);
+                previous = current;
+            }
+            current = current.next;
+        }
+    }
+	  
+ 
+	public void display() {
+     Node temp = head;
+     while(temp!= null) {
+    	 System.out.print(temp.data);
+    	 temp = temp.next;
+     }
+		
+	}
+	
+	
+}
---
+++ src/testVagrant/Main.java
@@ -0,0 +1,27 @@
+package testVagrant;
+
+
+
+import testVagrant.LinkedList.Node;
+
+public class Main {
+	
+		
+	public static void main( String []args ) {
+		LinkedList test = new LinkedList();
+		test.insertatBeginning("s1");
+		test.insertatBeginning("s2");
+		test.insertatBeginning("s3");
+		test.insertatBeginning("s4");
+		
+		LinkedList.removeDuplicate(null);
+		test.display();
+		
+		
+	}
+	
+}
+
+
+// 1 2 3 4 5
+//s2 s4 s3
